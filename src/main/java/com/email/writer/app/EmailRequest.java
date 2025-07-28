e684d4cfdf61ae197e6939772bf5d5119d7b10e5
rmkec111722203091 <119487400+rmkec111722203091@users.noreply.github.com>
2025-07-28 14:38:41 +0530

    Email-writer

---
+++ src/main/java/com/email/writer/app/EmailRequest.java
@@ -0,0 +1,35 @@
+package com.email.writer.app;
+
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+    private String emailContent;
+    private  String tone;
+
+    public String getEmailContent() {
+        return emailContent;
+    }
+
+    public void setEmailContent(String emailContent) {
+        this.emailContent = emailContent;
+    }
+
+    public String getTone() {
+        return tone;
+    }
+
+    public void setTone(String tone) {
+        this.tone = tone;
+    }
+
+    public EmailRequest() {
+    }
+
+    public EmailRequest(String emailContent, String tone) {
+        this.emailContent = emailContent;
+        this.tone = tone;
+    }
+}
+
+
